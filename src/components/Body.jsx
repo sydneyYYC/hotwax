0162d78f2e0d5f2e5069e10e0187d0ebcb28757f
Sydney Bruce <sydneybruce20@gmail.com>
2023-12-08 14:21:51 -0700

    api fetch to spodify and rendering playlists

---
+++ src/components/Body.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect } from "react";
+
+export default function Body() {
+  return <div>Body</div>
+}
--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Footer from "./Footer";
+import { useStateProvider } from "../utils/StateProvider";
+import Playlists from "./Playlists";
 
 export default function Spotify() {
-  return <div>Spotify</div>
+  // useStateProvider()
+  // useEffect(() => {
+
+  // }, [dispatch, token])
+  return <main className="max-w-screen min-h-screen bg-green-100">
+    <div className="m-4">Spotify</div>
+    <Playlists />
+    <Footer></Footer>
+    </main>
 }
+

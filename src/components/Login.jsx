620c445005cc6aaeb036952190d4efb57b14b789
Sydney Bruce <sydneybruce20@gmail.com>
2023-12-12 08:19:50 -0700

    changed uri redirect in login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ const  {REACT_APP_spotifyClientId} = process.env
 export default function Login() {
   const handleClick = () => {
     const clientId=`${REACT_APP_spotifyClientId}`;
-    const redirectUri = "http://localhost:3000";
+    const redirectUri = "http://https://hotwax.netlify.app/";
     const apiUrl = "https://accounts.spotify.com/authorize";
     const scope = ['user-read-email','user-read-private', 'user-modify-playback-state', 'user-read-playback-state', 'user-read-currently-playing', 'user-read-recently-played', 'user-read-playback-position', 'user-top-read'];
     window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join(" ")}&response_type=token&show_dialog=true`;
